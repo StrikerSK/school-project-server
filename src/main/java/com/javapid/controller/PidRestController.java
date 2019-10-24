376a38a8ce230cac088638e5cd2697b177b5e526
StrikerSK <kristianhanus@gmail.com>
2019-10-24 16:54:49 +0200

    First template query to database

--- src/main/java/com/javapid/controller/PidRestController.java
+++ src/main/java/com/javapid/controller/PidRestController.java
@@ -1,7 +1,10 @@
 package com.javapid.controller;
 
 import com.javapid.entity.CouponEntity;
+import com.javapid.entity.nivo.DataXY;
+import com.javapid.repository.CouponRepository;
 import com.javapid.service.PidService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -16,14 +20,17 @@ public class PidRestController {
 
 	private final PidService pidService;
 
+	@Autowired
+	private CouponRepository couponRepository;
+
 	public PidRestController(PidService pidService) {
 		this.pidService = pidService;
 	}
 
-	@RequestMapping(name = "/getData")
-	public List<CouponEntity> getData() {
-		return pidService.getAllData();
-	}
+//	@RequestMapping(name = "/getData")
+//	public List<CouponEntity> getData() {
+//		return pidService.getAllData();
+//	}
 
 	@RequestMapping("/uploadJson")
 	public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
@@ -33,6 +40,11 @@ public class PidRestController {
 	@RequestMapping("/getData/{code}")
 	public List<CouponEntity> uploadFile(@PathVariable String code) {
 		return pidService.getDataByCode(code);
+	}
+
+	@RequestMapping(name = "/test")
+	public List<DataXY> testEndpoint() {
+		return couponRepository.testTemplating("dospeli", Arrays.asList("Mesačná"));
 	}
 
 }
