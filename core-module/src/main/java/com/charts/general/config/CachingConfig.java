9d588fda17205d1f5155c576c2956178a2294289
Teh|Striker <kristianhanus@gmail.com>
2022-08-01 21:10:20 +0200

    Initialize caching (#25)

---
+++ core-module/src/main/java/com/charts/general/config/CachingConfig.java
@@ -0,0 +1,17 @@
+package com.charts.general.config;
+
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CachingConfig implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
+
+    @Override
+    public void customize(ConcurrentMapCacheManager cacheManager) {
+        cacheManager.setCacheNames(Arrays.asList("couponList", "ticketList"));
+    }
+
+}
