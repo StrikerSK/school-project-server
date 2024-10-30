fb8d12c333726cef9118f4fcfac536e1021b6448
Teh|Striker <kristianhanus@gmail.com>
2024-10-30 23:32:23 +0100

    New coupon entity class mapping

--- core-module/src/main/java/com/charts/general/entity/coupon/updated/UpdateCouponEntity.java
+++ core-module/src/main/java/com/charts/general/entity/coupon/updated/UpdateCouponEntity.java
@@ -1,31 +1,51 @@
 package com.charts.general.entity.coupon.updated;
 
 import com.charts.general.entity.AbstractUpdateEntity;
+import com.charts.general.entity.enums.Months;
 import com.charts.general.entity.enums.PersonType;
 import com.charts.general.entity.enums.SellType;
 import com.charts.general.entity.enums.Validity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "updated_coupons")
+@Table(name = "coupon_entity_v2")
 @NoArgsConstructor
 @Getter
 @Setter
 public class UpdateCouponEntity extends AbstractUpdateEntity {
 
-	@Column(name = "platnost")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	@JsonIgnore
+	private Long id;
+
+	@Column(name = "code")
+	@JsonProperty("code")
+	private String code;
+
+	@Column(name = "month")
+	private Months month;
+
+	@Column(name = "year")
+	private Integer year;
+
+	@Column(name = "value")
+	private Integer value;
+
+	@Column(name = "validity")
 	private Validity validity;
 
-	@Column(name = "typ_predaja")
+	@Column(name = "sellType")
 	private SellType sellType;
 
-	@Column(name = "typ_osoby")
+	@Column(name = "personType")
 	private PersonType personType;
 
 }
