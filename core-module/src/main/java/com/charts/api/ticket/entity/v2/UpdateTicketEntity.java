c8650ef6fc61ca1334740d8a0413b79108a962a4
Teh|Striker <kristianhanus@gmail.com>
2024-12-23 21:57:13 +0100

    Introduce new table

--- core-module/src/main/java/com/charts/api/ticket/entity/v2/UpdateTicketEntity.java
+++ core-module/src/main/java/com/charts/api/ticket/entity/v2/UpdateTicketEntity.java
@@ -1,26 +1,46 @@
 package com.charts.api.ticket.entity.v2;
 
 import com.charts.general.entity.AbstractUpdateEntity;
+import com.charts.general.entity.enums.Months;
 import com.charts.general.entity.enums.TicketTypes;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "updated_tickets")
+@Table(name = "tickets_v2")
 @NoArgsConstructor
 @Getter
 @Setter
 public class UpdateTicketEntity extends AbstractUpdateEntity implements Comparable<UpdateTicketEntity> {
 
-	@Column(name = "typ_listka")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	@JsonIgnore
+	private Long id;
+
+	@Column(name = "code")
+	@JsonProperty("code")
+	private String code;
+
+	@Column(name = "month")
+	private Months month;
+
+	@Column(name = "year")
+	private Integer year;
+
+	@Column(name = "value")
+	private Integer value;
+
+	@Column(name = "ticket_type")
 	private TicketTypes ticketType;
 
-	@Column(name = "zlavneny")
+	@Column(name = "discounted")
 	private Boolean discounted;
 
 	@Override
