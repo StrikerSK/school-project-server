ebc4ad77407962b7b09d747b4cb1d440eb0abc0b
Teh|Striker <kristianhanus@gmail.com>
2024-12-28 14:33:38 +0100

    Rework Apex - Change Ticket controller

--- core-module/src/main/java/com/charts/general/utils/AbstractFunctionUtils.java
+++ core-module/src/main/java/com/charts/general/utils/AbstractFunctionUtils.java
@@ -1,6 +1,7 @@
 package com.charts.general.utils;
 
 import com.charts.general.exception.InvalidParameterException;
+import org.apache.commons.lang3.StringUtils;
 
 public abstract class AbstractFunctionUtils {
 
@@ -8,6 +9,10 @@ public abstract class AbstractFunctionUtils {
     public static final String YEAR_GROUP = "year";
 
     public static void validateGroups(String upperGroup, String lowerGroup) {
+        if (StringUtils.isEmpty(upperGroup) || StringUtils.isEmpty(lowerGroup)) {
+            throw new InvalidParameterException("Groups cannot be null nor empty");
+        }
+
         if (upperGroup.equals(lowerGroup)) {
             throw new InvalidParameterException("Cannot use same groups");
         }
