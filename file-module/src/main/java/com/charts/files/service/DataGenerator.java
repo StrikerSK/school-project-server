0da220a045a47bef0d4c66e2793997d853ea5071
Teh|Striker <kristianhanus@gmail.com>
2024-12-29 17:46:01 +0100

    Data Generator - Add Ticket generation

--- file-module/src/main/java/com/charts/files/service/DataGenerator.java
+++ file-module/src/main/java/com/charts/files/service/DataGenerator.java
@@ -4,12 +4,16 @@ import com.charts.api.coupon.entity.v2.UpdateCouponEntity;
 import com.charts.api.coupon.enums.types.PersonType;
 import com.charts.api.coupon.enums.types.SellType;
 import com.charts.api.coupon.enums.types.Validity;
+import com.charts.api.ticket.entity.v2.UpdateTicketEntity;
+import com.charts.api.ticket.enums.TicketType;
 import com.charts.general.entity.enums.EnumUtils;
 import com.charts.general.entity.enums.IEnum;
 import com.charts.general.entity.enums.types.Months;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DataGenerator {
 
@@ -17,14 +21,32 @@ public class DataGenerator {
         List<UpdateCouponEntity> enitityList = new java.util.ArrayList<>();
 
         for (int i = 0; i < counts; i++) {
-            UpdateCouponEntity updateCouponEntity = new UpdateCouponEntity();
-            updateCouponEntity.setMonth(getRandomElement(Months.class));
-            updateCouponEntity.setSellType(getRandomElement(SellType.class));
-            updateCouponEntity.setValidity(getRandomElement(Validity.class));
-            updateCouponEntity.setPersonType(getRandomElement(PersonType.class));
-            updateCouponEntity.setYear(generateYear());
-            updateCouponEntity.setValue(new Random().nextInt(1000000));
-            enitityList.add(updateCouponEntity);
+            UpdateCouponEntity coupon = UpdateCouponEntity.builder()
+                    .year(generateYear())
+                    .month(getRandomElement(Months.class))
+                    .personType(getRandomElement(PersonType.class))
+                    .validity(getRandomElement(Validity.class))
+                    .sellType(getRandomElement(SellType.class))
+                    .value(new Random().nextInt(1000000))
+                    .build();
+            enitityList.add(coupon);
+        }
+
+        return enitityList;
+    }
+
+    public static List<UpdateTicketEntity> generateTickets(Integer counts) {
+        List<UpdateTicketEntity> enitityList = new java.util.ArrayList<>();
+        List<Boolean> discounted = Stream.of(true, false).collect(Collectors.toList());
+
+        for (int i = 0; i < counts; i++) {
+            UpdateTicketEntity ticket = UpdateTicketEntity.builder()
+                    .year(generateYear())
+                    .month(getRandomElement(Months.class))
+                    .ticketType(getRandomElement(TicketType.class))
+                    .discounted(getRandomElement(discounted))
+                    .build();
+            enitityList.add(ticket);
         }
 
         return enitityList;
