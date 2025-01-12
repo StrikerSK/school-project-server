61d27996f408b29f7f8e97790d1b75821d421d9b
Teh|Striker <kristianhanus@gmail.com>
2025-01-12 10:59:24 +0100

    Apex Test - Unify tests

--- apex-module/src/test/java/com/charts/apex/service/ApexCouponServiceTest.java
+++ apex-module/src/test/java/com/charts/apex/service/ApexCouponServiceTest.java
@@ -2,89 +2,84 @@ package com.charts.apex.service;
 
 import com.charts.apex.entity.ApexObject;
 import com.charts.api.coupon.entity.CouponsParameters;
+import com.charts.api.coupon.entity.v2.UpdateCouponEntity;
+import com.charts.api.coupon.enums.types.PersonType;
+import com.charts.api.coupon.enums.types.SellType;
+import com.charts.api.coupon.enums.types.Validity;
+import com.charts.api.coupon.service.CouponV2Service;
 import com.charts.api.coupon.utils.CouponFunctionUtils;
-import org.junit.jupiter.api.Test;
+import com.charts.general.entity.enums.types.Months;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static com.charts.general.entity.enums.types.Months.*;
+import static org.mockito.Mockito.when;
 
-public class ApexCouponServiceTest extends AbstractApexCouponServiceTest {
+public class ApexCouponServiceTest {
+
+    @Mock
+    private CouponV2Service couponV2Service;
+
+    @InjectMocks
+    private ApexCouponService apexCouponService;
+
+    private AutoCloseable closeable;
+    private final CouponsParameters couponsParameters = new CouponsParameters(Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList());
+
 
     @BeforeClass
     public void setup() {
-        super.setup();
+        UpdateCouponEntity couponEntity1 = UpdateCouponEntity.builder().id(123L).value(100).month(JANUARY).year(2015).validity(Validity.FIVE_MONTHS).sellType(SellType.COUPON).personType(PersonType.ADULT).build();
+        UpdateCouponEntity couponEntity2 = UpdateCouponEntity.builder().id(123L).value(200).month(JANUARY).year(2016).validity(Validity.MONTHLY).sellType(SellType.COUPON).personType(PersonType.ADULT).build();
+
+        UpdateCouponEntity couponEntity3 = UpdateCouponEntity.builder().id(123L).value(300).month(MARCH).year(2016).validity(Validity.FIVE_MONTHS).sellType(SellType.CARD).personType(PersonType.ADULT).build();
+        UpdateCouponEntity couponEntity4 = UpdateCouponEntity.builder().id(123L).value(400).month(MARCH).year(2015).validity(Validity.MONTHLY).sellType(SellType.ESHOP).personType(PersonType.ADULT).build();
+
+        UpdateCouponEntity couponEntity5 = UpdateCouponEntity.builder().id(123L).value(500).month(Months.FEBRUARY).year(2016).validity(Validity.YEARLY).sellType(SellType.CARD).personType(PersonType.SENIOR).build();
+        UpdateCouponEntity couponEntity6 = UpdateCouponEntity.builder().id(123L).value(600).month(Months.FEBRUARY).year(2015).validity(Validity.MONTHLY).sellType(SellType.ESHOP).personType(PersonType.SENIOR).build();
+
+        List<UpdateCouponEntity> couponList = new ArrayList<>();
+        couponList.add(couponEntity1);
+        couponList.add(couponEntity2);
+        couponList.add(couponEntity3);
+        couponList.add(couponEntity4);
+        couponList.add(couponEntity5);
+        couponList.add(couponEntity6);
+
+        closeable = MockitoAnnotations.openMocks(this);
+        when(couponV2Service.findCouponEntities(Mockito.any())).thenReturn(couponList);
     }
 
     @Test
     public void generalDataRetrieval() {
-        CouponsParameters couponsParameters = new CouponsParameters(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
         List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
+        Assert.assertEquals(result.size(), 12);
 
-        Assert.assertEquals(result.size(), 6);
-        Assert.assertEquals(result.get(0).getValues().stream().reduce(0, Integer::sum), 1000);
+        Assert.assertEquals(getApexObject(JANUARY.getValue(), result), 300);
+        Assert.assertEquals(getApexObject(FEBRUARY.getValue(), result), 1100);
+        Assert.assertEquals(getApexObject(MARCH.getValue(), result), 700);
     }
 
-//    @Test
-//    public void retrieveDataByValidityType() {
-//        List<String> validityType = new ArrayList<>();
-//        validityType.add(Validity.FIVE_MONTHS.getValue());
-//
-//        CouponsParameters couponsParameters = new CouponsParameters(validityType, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-//        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-//
-//        assertEquals(6, result.size());
-//        assertEquals(400, result.get(0).getValues().stream().reduce(0, Integer::sum));
-//    }
-//
-//    @Test
-//    public void retrieveDataBySellType() {
-//        List<String> sellTypes = new ArrayList<>();
-//        sellTypes.add(SellType.ESHOP.getValue());
-//
-//        CouponsParameters couponsParameters = new CouponsParameters(new ArrayList<>(), sellTypes, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-//        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-//
-//        assertEquals(6, result.size());
-//        assertEquals(400, result.get(0).getValues().stream().reduce(0, Integer::sum));
-//    }
-//
-//    @Test
-//    public void retrieveDataByMonth() {
-//        List<String> months = new ArrayList<>();
-//        months.add(Months.MARCH.getValue());
-//
-//        CouponsParameters couponsParameters = new CouponsParameters(new ArrayList<>(), new ArrayList<>(), months, new ArrayList<>(), new ArrayList<>());
-//        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-//
-//        assertEquals(6, result.size());
-//        assertEquals(700, result.get(0).getValues().stream().reduce(0, Integer::sum));
-//    }
-//
-//    @Test
-//    public void retrieveDataByYear() {
-//        List<Integer> years = new ArrayList<>();
-//        years.add(2016);
-//
-//        CouponsParameters couponsParameters = new CouponsParameters(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), years, new ArrayList<>());
-//        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-//
-//        assertEquals(6, result.size());
-//        assertEquals(500, result.get(0).getValues().stream().reduce(0, Integer::sum));
-//    }
-//
-//    @Test
-//    public void retrieveDataByPersonType() {
-//        List<String> personTypes = new ArrayList<>();
-//        personTypes.add(PersonType.SENIOR.getValue());
-//
-//        CouponsParameters couponsParameters = new CouponsParameters(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), personTypes);
-//        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-//
-//        assertEquals(1, result.size());
-//        assertEquals(1100, result.get(0).getValues().stream().reduce(0, Integer::sum));
-//    }
+    private Integer getApexObject(String value, List<ApexObject> values) {
+        ApexObject apexObject = values.stream()
+                .filter(o -> o.getName().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(String.format("ApexObject not found for %s", value)));
+        return apexObject.getValues().stream().reduce(0, Integer::sum);
+    }
 
+    @AfterClass
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
 }
