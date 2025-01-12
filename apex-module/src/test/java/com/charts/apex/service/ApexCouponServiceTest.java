ac17ac5c37442fdcbad1c98bcf6f5dee7169300a
Teh|Striker <kristianhanus@gmail.com>
2025-01-12 21:13:29 +0100

    Apex Test - Extend testing

--- apex-module/src/test/java/com/charts/apex/service/ApexCouponServiceTest.java
+++ apex-module/src/test/java/com/charts/apex/service/ApexCouponServiceTest.java
@@ -3,7 +3,6 @@ package com.charts.apex.service;
 import com.charts.apex.entity.ApexObject;
 import com.charts.api.coupon.entity.CouponsParameters;
 import com.charts.api.coupon.entity.v2.UpdateCouponEntity;
-import com.charts.api.coupon.enums.types.PersonType;
 import com.charts.api.coupon.service.CouponV2Service;
 import com.charts.api.coupon.utils.CouponFunctionUtils;
 import org.mockito.InjectMocks;
@@ -19,6 +18,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.charts.api.coupon.enums.types.PersonType.*;
 import static com.charts.api.coupon.enums.types.SellType.*;
 import static com.charts.api.coupon.enums.types.Validity.*;
 import static com.charts.general.entity.constants.EnumerationConstants.MONTH_VALUES;
@@ -39,23 +39,23 @@ public class ApexCouponServiceTest {
 
     @BeforeClass
     public void setup() {
-        UpdateCouponEntity couponEntity1 = UpdateCouponEntity.builder().id(123L).value(100).month(JANUARY).year(2015).validity(FIVE_MONTHS).sellType(COUPON).personType(PersonType.ADULT).build();
-        UpdateCouponEntity couponEntity2 = UpdateCouponEntity.builder().id(123L).value(200).month(JANUARY).year(2016).validity(MONTHLY).sellType(ESHOP).personType(PersonType.ADULT).build();
+        UpdateCouponEntity couponEntity1 = UpdateCouponEntity.builder().id(123L).value(100).month(JANUARY).year(2015).validity(FIVE_MONTHS).sellType(COUPON).personType(ADULT).build();
+        UpdateCouponEntity couponEntity2 = UpdateCouponEntity.builder().id(123L).value(200).month(JANUARY).year(2016).validity(MONTHLY).sellType(ESHOP).personType(ADULT).build();
 
-        UpdateCouponEntity couponEntity3 = UpdateCouponEntity.builder().id(123L).value(300).month(MARCH).year(2016).validity(FIVE_MONTHS).sellType(CARD).personType(PersonType.ADULT).build();
-        UpdateCouponEntity couponEntity4 = UpdateCouponEntity.builder().id(123L).value(400).month(MARCH).year(2015).validity(MONTHLY).sellType(COUPON).personType(PersonType.ADULT).build();
+        UpdateCouponEntity couponEntity3 = UpdateCouponEntity.builder().id(123L).value(300).month(MARCH).year(2016).validity(FIVE_MONTHS).sellType(CARD).personType(ADULT).build();
+        UpdateCouponEntity couponEntity4 = UpdateCouponEntity.builder().id(123L).value(400).month(MARCH).year(2015).validity(MONTHLY).sellType(COUPON).personType(ADULT).build();
 
-        UpdateCouponEntity couponEntity5 = UpdateCouponEntity.builder().id(123L).value(500).month(FEBRUARY).year(2016).validity(YEARLY).sellType(CARD).personType(PersonType.SENIOR).build();
-        UpdateCouponEntity couponEntity6 = UpdateCouponEntity.builder().id(123L).value(600).month(FEBRUARY).year(2015).validity(MONTHLY).sellType(ESHOP).personType(PersonType.SENIOR).build();
+        UpdateCouponEntity couponEntity5 = UpdateCouponEntity.builder().id(123L).value(500).month(FEBRUARY).year(2016).validity(YEARLY).sellType(CARD).personType(SENIOR).build();
+        UpdateCouponEntity couponEntity6 = UpdateCouponEntity.builder().id(123L).value(600).month(FEBRUARY).year(2015).validity(MONTHLY).sellType(ESHOP).personType(SENIOR).build();
 
-        UpdateCouponEntity couponEntity7 = UpdateCouponEntity.builder().id(123L).value(100).month(JANUARY).year(2015).validity(FIVE_MONTHS).sellType(COUPON).personType(PersonType.ADULT).build();
-        UpdateCouponEntity couponEntity8 = UpdateCouponEntity.builder().id(123L).value(200).month(JANUARY).year(2016).validity(MONTHLY).sellType(ESHOP).personType(PersonType.ADULT).build();
+        UpdateCouponEntity couponEntity7 = UpdateCouponEntity.builder().id(123L).value(100).month(JANUARY).year(2015).validity(FIVE_MONTHS).sellType(COUPON).personType(ADULT).build();
+        UpdateCouponEntity couponEntity8 = UpdateCouponEntity.builder().id(123L).value(200).month(JANUARY).year(2016).validity(MONTHLY).sellType(ESHOP).personType(ADULT).build();
 
-        UpdateCouponEntity couponEntity9 = UpdateCouponEntity.builder().id(123L).value(300).month(MARCH).year(2016).validity(FIVE_MONTHS).sellType(CARD).personType(PersonType.ADULT).build();
-        UpdateCouponEntity couponEntity10 = UpdateCouponEntity.builder().id(123L).value(400).month(MARCH).year(2015).validity(MONTHLY).sellType(COUPON).personType(PersonType.ADULT).build();
+        UpdateCouponEntity couponEntity9 = UpdateCouponEntity.builder().id(123L).value(300).month(MARCH).year(2016).validity(FIVE_MONTHS).sellType(CARD).personType(ADULT).build();
+        UpdateCouponEntity couponEntity10 = UpdateCouponEntity.builder().id(123L).value(400).month(MARCH).year(2015).validity(MONTHLY).sellType(COUPON).personType(ADULT).build();
 
-        UpdateCouponEntity couponEntity11 = UpdateCouponEntity.builder().id(123L).value(500).month(FEBRUARY).year(2016).validity(YEARLY).sellType(CARD).personType(PersonType.SENIOR).build();
-        UpdateCouponEntity couponEntity12 = UpdateCouponEntity.builder().id(123L).value(600).month(FEBRUARY).year(2015).validity(MONTHLY).sellType(ESHOP).personType(PersonType.SENIOR).build();
+        UpdateCouponEntity couponEntity11 = UpdateCouponEntity.builder().id(123L).value(500).month(FEBRUARY).year(2016).validity(YEARLY).sellType(CARD).personType(SENIOR).build();
+        UpdateCouponEntity couponEntity12 = UpdateCouponEntity.builder().id(123L).value(600).month(FEBRUARY).year(2015).validity(MONTHLY).sellType(ESHOP).personType(SENIOR).build();
 
         List<UpdateCouponEntity> couponList = List.of(
                 couponEntity1, couponEntity2, couponEntity3,
@@ -76,7 +76,8 @@ public class ApexCouponServiceTest {
     @Test
     public void retrieveData_yearlyPerPersonType() {
         List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.YEAR_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-        Assert.assertEquals(result.size(), 2);
+        List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
+        Assert.assertEquals(resultValues, List.of("2015", "2016"));
 
         ApexObject apexObject1 = getApexObject("2015", result);
         Assert.assertEquals(getApexObjectSum("2015", result), 2200);
@@ -92,7 +93,8 @@ public class ApexCouponServiceTest {
     @Test
     public void retrieveData_yearlyPerSellType() {
         List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.YEAR_GROUP, CouponFunctionUtils.SELL_GROUP, couponsParameters);
-        Assert.assertEquals(result.size(), 2);
+        List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
+        Assert.assertEquals(resultValues, List.of("2015", "2016"));
 
         ApexObject apexObject1 = getApexObject("2015", result);
         Assert.assertEquals(getApexObjectSum("2015", result), 2200);
@@ -108,8 +110,6 @@ public class ApexCouponServiceTest {
     @Test
     public void retrieveData_yearlyPerValidity() {
         List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.YEAR_GROUP, CouponFunctionUtils.VALIDITY_GROUP, couponsParameters);
-        Assert.assertEquals(result.size(), 2);
-
         List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
         Assert.assertEquals(resultValues, List.of("2015", "2016"));
 
@@ -127,8 +127,6 @@ public class ApexCouponServiceTest {
     @Test
     public void retrieveData_monthly() {
         List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.MONTH_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-        Assert.assertEquals(result.size(), 12);
-
         List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
         Assert.assertEquals(resultValues, MONTH_VALUES);
 
@@ -147,16 +145,27 @@ public class ApexCouponServiceTest {
     }
 
     @Test
-    public void retrieveData_validity() {
-        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.VALIDITY_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
-        Assert.assertEquals(result.size(), 3);
+    public void retrieveData_personPerYear() {
+        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.PERSON_GROUP, CouponFunctionUtils.YEAR_GROUP, couponsParameters);
+        List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
+        Assert.assertEquals(resultValues, List.of(ADULT.getValue(), SENIOR.getValue()));
 
+        ApexObject apexObject1 = getApexObject(ADULT.getValue(), result);
+        Assert.assertEquals(getApexObjectSum(ADULT.getValue(), result), 2000);
+        Assert.assertEquals(apexObject1.getValues().size(), 2);
+        Assert.assertEquals(apexObject1.getValues(), List.of(1000, 1000));
+
+        ApexObject apexObject2 = getApexObject(SENIOR.getValue(), result);
+        Assert.assertEquals(getApexObjectSum(SENIOR.getValue(), result), 2200);
+        Assert.assertEquals(apexObject2.getValues().size(), 2);
+        Assert.assertEquals(apexObject2.getValues(), List.of(1200, 1000));
+    }
+
+    @Test
+    public void retrieveData_validityPerPerson() {
+        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.VALIDITY_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
         List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
         Assert.assertEquals(resultValues, List.of(MONTHLY.getValue(), FIVE_MONTHS.getValue(), YEARLY.getValue()));
-
-        Assert.assertEquals(getApexObjectSum(MONTHLY.getValue(), result), 2400);
-        Assert.assertEquals(getApexObjectSum(FIVE_MONTHS.getValue(), result), 800);
-        Assert.assertEquals(getApexObjectSum(YEARLY.getValue(), result), 1000);
 
         ApexObject apexObject1 = getApexObject(MONTHLY.getValue(), result);
         Assert.assertEquals(getApexObjectSum(MONTHLY.getValue(), result), 2400);
@@ -172,6 +181,28 @@ public class ApexCouponServiceTest {
         Assert.assertEquals(getApexObjectSum(YEARLY.getValue(), result), 1000);
         Assert.assertEquals(apexObject3.getValues().size(), 1);
         Assert.assertEquals(apexObject3.getValues(), List.of(1000));
+    }
+
+    @Test
+    public void retrieveData_sellTypePerPerson() {
+        List<ApexObject> result = apexCouponService.getCouponData(CouponFunctionUtils.SELL_GROUP, CouponFunctionUtils.PERSON_GROUP, couponsParameters);
+        List<String> resultValues = result.stream().map(ApexObject::getName).collect(Collectors.toList());
+        Assert.assertEquals(resultValues, List.of(CARD.getValue(), COUPON.getValue(), ESHOP.getValue()));
+
+        ApexObject apexObject1 = getApexObject(COUPON.getValue(), result);
+        Assert.assertEquals(getApexObjectSum(COUPON.getValue(), result), 1000);
+        Assert.assertEquals(apexObject1.getValues().size(), 1);
+        Assert.assertEquals(apexObject1.getValues(), List.of(1000));
+
+        ApexObject apexObject2 = getApexObject(ESHOP.getValue(), result);
+        Assert.assertEquals(getApexObjectSum(CARD.getValue(), result), 1600);
+        Assert.assertEquals(apexObject2.getValues().size(), 2);
+        Assert.assertEquals(apexObject2.getValues(), List.of(400, 1200));
+
+        ApexObject apexObject3 = getApexObject(CARD.getValue(), result);
+        Assert.assertEquals(getApexObjectSum(CARD.getValue(), result), 1600);
+        Assert.assertEquals(apexObject3.getValues().size(), 2);
+        Assert.assertEquals(apexObject3.getValues(), List.of(600, 1000));
     }
 
     private Integer getApexObjectSum(String searchedValue, List<ApexObject> values) {
